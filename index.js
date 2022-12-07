2e6381b60f7caee4bf9aa3f8ffabb761a581fe07
Maria Dayekh <mariaaldayekh@gmail.com>
2022-12-07 21:25:22 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+const nameArray=[];
+const names= ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names,event){
+ for (let i=0; i<names.length ; i++){
+    nameArray.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);  
+ }
+ return nameArray;
+};
+
+console.log(writeCards(names,'surprise'));
+
+
+for (let age = 30; age < 40; age++){
+   console.log(`I'm ${age} years old. Happe birthday to me!`);
+
+};
+
+function countDown(i){
+   while  (i>=0){
+      console.log(i--);
+   }
+};
+
+countDown(4);
